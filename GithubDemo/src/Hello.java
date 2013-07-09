1c37fdf9a76f33fd5b34c645335b26a0857f1757
Austin <zyjbupt@gmail.com>
2013-07-09 14:22:08 +0800

    bbbbbbbbbbbbbbb

--- GithubDemo/src/Hello.java
+++ GithubDemo/src/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 	public static void main(String[] args)
 	{
 		System.out.println("Helloworld yunjiez ooooooooooooooooooooooo!");
-		System.out.println("2 Helloworld yunjiez ooooooooooooooooooooooo!");
+		System.out.println("4444444444 Helloworld yunjiez ooooooooooooooooooooooo!");
 	}
 }
